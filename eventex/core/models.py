223e732b281a74f0e416d76ff8dc656dc9a7f12b
Alessandro Fernandes <alcfernandes@yahoo.com>
2016-10-07 21:09:19 -0300

    Course como Abstract Base Class

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -44,7 +44,7 @@ class Contact(models.Model):
         return self.value
 
 
-class Talk(models.Model):
+class Activity(models.Model):
     title = models.CharField('título', max_length=200)
     start = models.TimeField('início', blank=True, null=True)
     description = models.TextField('descrição', blank=True)
@@ -53,6 +53,7 @@ class Talk(models.Model):
     objects = PeriodManager()
 
     class Meta:
+        abstract = True
         verbose_name = 'palestra'
         verbose_name_plural = 'palestras'
 
@@ -60,5 +61,13 @@ class Talk(models.Model):
         return self.title
 
 
+class Talk(Activity):
+    pass
 
 
+class Course(Activity):
+    slots = models.IntegerField('vagas')
+
+    class Meta:
+        verbose_name = 'curso'
+        verbose_name_plural = 'cursos'
